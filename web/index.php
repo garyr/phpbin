f2fdb96fa00b7be6638d77d6af4a506afbba5b7c
Gary Rogers <gmrwebde@gmail.com>
2015-02-27 17:01:10 -0700

    Fixes issue with composer autoloading

--- web/index.php
+++ web/index.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+if (is_dir(__DIR__.'/../vendor')) {
+    require_once __DIR__.'/../vendor/autoload.php';
+} else if(__DIR__.'/../vendor') {
+    require_once __DIR__.'/../../../autoload.php';
+} else {
+    throw new \Exception("Error loading composer");
+}
 
 $app = new Phpbin\Web\Application();
 $app->run();
